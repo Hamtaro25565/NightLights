ab33211485faeaff3f443e4a12a4b43b4490541e
Hamtaro25565 <46054134+Hamtaro25565@users.noreply.github.com>
2020-04-19 02:12:29 +0100

    Update TogetherAtHomeConcert.lua

--- TogetherAtHomeConcert.lua
+++ TogetherAtHomeConcert.lua
@@ -13,3 +13,6 @@ for i,v in pairs(game.Workspace.CollectablesClones:GetChildren()) do
         v.CollectablePart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
     end
 end
+
+print("Completed this script. Made by Hamtaro#2312 from Night Light Developments.")
+print("Quick word from Hamtaro; Hey guys, I hope you are all safe and enjoying your life.")
