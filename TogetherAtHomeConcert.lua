e39a1ca5ffdf66c2871dc2c73c0cceba7f3492cc
Hamtaro25565 <46054134+Hamtaro25565@users.noreply.github.com>
2020-04-19 02:11:09 +0100

    Update TogetherAtHomeConcert.lua
    Just a quick update because of inconsistency of the games.

--- TogetherAtHomeConcert.lua
+++ TogetherAtHomeConcert.lua
@@ -13,15 +13,3 @@ for i,v in pairs(game.Workspace.CollectablesClones:GetChildren()) do
         v.CollectablePart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
     end
 end
-
-for i,v in pairs(game.Workspace.CollectableModels:GetChildren()) do
-    if v:GetChildren() == BodyMeshPart then
-        v.BodyMeshPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    end
-end
-
-for i,v in pairs(game.Workspace.CollectableModels:GetChildren()) do
-    if v:GetChildren() == CollectablePart then
-        v.CollectablePart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    end
-end
