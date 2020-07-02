cef079089852c460006789616b8ba325a815b532
Hamtaro25565 <46054134+Hamtaro25565@users.noreply.github.com>
2020-07-03 00:39:24 +0100

    Updated script
    Better performance. Play with the numbers if you want to.

--- RoanokeVA.lua
+++ RoanokeVA.lua
@@ -2,8 +2,11 @@ local NL_Plr = game.Players.LocalPlayer.Name
 print("This script is made by Night Lights Development team member, Hamtaro#2312. If anyone else claims to have made this script, they are lying.")
 for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
     if v.Name == (NL_Plr .. "'s Car") then
-        local NL_Thrust = Instance.new("BodyForce", v.DriveSeat)
-        v.DriveSeat.BodyForce.Force =  Vector3.new(0,1000000,0)
+        local NL_Thrust = Instance.new("BodyPosition", v.DriveSeat)
+        v.DriveSeat.BodyPosition.Position =  Vector3.new(0, 999999999999999999, 0)
+        v.DriveSeat.BodyPosition.MaxForce =  Vector3.new(0, 999999999, 0)
+        v.DriveSeat.BodyPosition.P =  999999
+        v.DriveSeat.BodyPosition.D =  0
         print("Started car force. Presented to you by Night Light Developments team!")
     end
 end
